81b532b1c860d89aee22248a77552caf215e251d
teogen404 <gartungaleksandr4@gmail.com>
2022-10-28 10:20:51 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
+let wasOpen = 1;
 function openMenu(){
     document.getElementById("sidebar").classList.toggle('active');
     document.querySelector('.header__list').classList.toggle('active');
+    wasOpen++;
 }
+let element = document.getElementById("idn1");
 
+window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY;
+    let elementEnd = element.offsetHeight - 75;
+    if(scrollTop >= elementEnd && wasOpen % 2 == 0){
+        document.getElementById("sidebar").classList.remove('active');
+        document.querySelector('.header__list').classList.remove('active');
+    }
+    if(scrollTop <= elementEnd && wasOpen % 2 == 0){
+        document.getElementById("sidebar").classList.add('active');
+        document.querySelector('.header__list').classList.add('active');
+    }
+});
